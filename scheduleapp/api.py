a01a4786ececc348a73dfea5667e47ed00eb9712
andrew_sh <andrew.sh@razortheory.com>
2017-04-24 16:51:37 +0300

    Added case when you haven't any lessons for requested group

--- scheduleapp/api.py
+++ scheduleapp/api.py
@@ -3,9 +3,9 @@ from django.contrib.auth.models import User
 from rest_framework import mixins
 from rest_framework import viewsets
 
-from .models import Lesson, LessonDate
+from .models import Lesson, LessonDate, Group
 from .serializers import UserSerializer, LessonSerializer, DaySerializer
-
+from .bsuir_lessons import get_schedule_for
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
@@ -22,5 +22,7 @@ class ScheduleViewSet(viewsets.ReadOnlyModelViewSet):
         date_to = self.request.query_params.get('date_to')
         date_to = datetime.datetime.strptime(date_to, '%Y-%m-%d').date()
         group_id = int(self.request.query_params.get('group_id'))
+        if LessonDate.objects.filter(group_id=group_id).count() == 0:
+            get_schedule_for(Group.objects.get(id=group_id).local_id)
         items = LessonDate.objects.filter(date__gte=date_from, date__lt=date_to, group_id=group_id).order_by('date')
         return items
