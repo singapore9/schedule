4b008c93352037beba75183f1615379fe147d2dd
andrew_sh <andrew.sh@razortheory.com>
2017-04-25 22:06:04 +0300

    Added fields to User

--- custom_auth/models.py
+++ custom_auth/models.py
@@ -1,12 +1,21 @@
 from django.contrib.auth.base_user import AbstractBaseUser
 from django.db import models
 
+from scheduleapp.models import Lesson
+
 
 class ApplicationUser(AbstractBaseUser):
     username = models.CharField(null=False, max_length=256)
     email = models.EmailField(unique=True, blank=False, null=False)
+    first_name = models.CharField(null=True, max_length=256)
+    last_name = models.CharField(null=True, max_length=256)
+    visited_lessons = models.ManyToManyField(Lesson, related_name='visitors')
+    unvisited_lessons = models.ManyToManyField(Lesson, related_name='slackers')
 
     USERNAME_FIELD = 'email'
+
+    def __str__(self):
+        return '[%s] %s %s' % (self.email, self.last_name, self.first_name)
 
 
 class Token(models.Model):
